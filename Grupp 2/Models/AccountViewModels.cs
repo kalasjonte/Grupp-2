918ad8c301ab7f79005ca534d0b5d1de838e454c
Kalasjonte <jonathan.gabrielsson@live.se>
2021-12-14 15:27:51 +0100

    a

--- Grupp 2/Models/AccountViewModels.cs
+++ Grupp 2/Models/AccountViewModels.cs
@@ -20,6 +20,7 @@ namespace Grupp_2.Models
         public string SelectedProvider { get; set; }
         public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
         public string ReturnUrl { get; set; }
+
         public bool RememberMe { get; set; }
     }
 
