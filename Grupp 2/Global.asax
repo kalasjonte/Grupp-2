721ef02e00fdfb0e7ecf3b8fa5490388596375b0
Kalasjonte <jonathan.gabrielsson@live.se>
2021-12-14 14:34:14 +0100

    Add project files.

---
+++ Grupp 2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Grupp_2.MvcApplication" Language="C#" %>
---
+++ Grupp 2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Grupp_2.Startup))]
+namespace Grupp_2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
