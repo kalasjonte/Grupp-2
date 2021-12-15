7a9dbbea8001aa7fe984ce56cfa89bd01cdab912
wootex <76428102+wootex@users.noreply.github.com>
2021-12-15 15:48:14 +0100

    noob grej

---
+++ Data/Models/Education.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models
+{
+    public class Education
+    {
+        [Key]
+        public int EduID { get; set; }
+        public string Title { get; set; }
+    }
+}
