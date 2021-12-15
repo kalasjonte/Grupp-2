94a0cacec8daea1e8dde97730b08f89aa7b8c6ee
Noel Lindgren <apexlindgren@hotmail.com>
2021-12-15 15:53:40 +0100

    Modeller School och School_type

---
+++ Data/Models/School.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models
+{
+    public class School
+    {
+        [Key]
+        public int SchoolID { get; set; }
+        public string Name { get; set; }
+        public string Place { get; set; }
+    }
+}
---
+++ Data/Models/School_Type.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.Models
+{
+    class School_Type
+    {
+        [Key]
+        public int School_TypeID { get; set; }
+        public string Type { get; set; }
+    }
+}
