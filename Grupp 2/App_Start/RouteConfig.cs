d3bc469c10230575e61466844ff0c556bfbf2abc
Noel Lindgren <apexlindgren@hotmail.com>
2021-12-15 10:41:25 +0100

    Kommentarer på Rout...(testar om kommentrarer kommer med i git)

--- Grupp 2/App_Start/RouteConfig.cs
+++ Grupp 2/App_Start/RouteConfig.cs
@@ -13,8 +13,13 @@ namespace Grupp_2
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //Här har vi en Rout med namnet "Default". En router ansvarar för att skapa kopplingen mellan rätt controller till inkommande HTTP-request.
             routes.MapRoute(
                 name: "Default",
+                //Om en URL matchar med detta URL-mönstret:
+                //Första delen "{controller}" antas vara namnet på controller:n
+                //Andra delen "{action}" antas vara namnet på action:en (controllerns method som ska hantera http-requesten)
+                //Tredje delen "{id}" är ett ID som vi kan passa till action:en
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
