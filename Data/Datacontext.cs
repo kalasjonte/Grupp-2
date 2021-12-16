a0e09e6c9e2b0b9be70a190a328adc37d203109a
wootex <76428102+wootex@users.noreply.github.com>
2021-12-16 11:50:15 +0100

    Revert "Revert "db con kanske""
    This reverts commit 58c6bf19a424190504ddc342edf01d40fadfd351.

--- Data/Datacontext.cs
+++ Data/Datacontext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,5 +10,21 @@ namespace Data
 {
     public class Datacontext : DbContext
     {
+        public Datacontext() // : base("Datacontext")
+        {
+
+        }
+        public DbSet<CV> CVs { get; set; }
+        public DbSet<Education> Educations { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<School> Schools { get; set; }
+        public DbSet<School_Type> School_Types { get; set; }
+
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<User_Message> User_Messages { get; set; }
+        public DbSet<Work_Experience> Work_Experiences { get; set; }
+
     }
 }
