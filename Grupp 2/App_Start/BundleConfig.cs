ce412e147418b75c854cdc373fd43aeec7baa41b
Kalasjonte <jonathan.gabrielsson@live.se>
2022-01-04 17:42:34 +0100

    css slit

--- Grupp 2/App_Start/BundleConfig.cs
+++ Grupp 2/App_Start/BundleConfig.cs
@@ -23,8 +23,11 @@ namespace Grupp_2
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                        "~/Content/bootstrap.css",
+                        "~/Content/bootstrap-lux.css",
+                      "~/Content/Site.css"));
+
+          
         }
     }
 }
