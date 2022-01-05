3c430a02f671f3bf70d25febb323f2ed1cd6ab30
wootex <76428102+wootex@users.noreply.github.com>
2022-01-05 14:06:29 +0100

    Education städ

--- Grupp 2/Controllers/EducationController.cs
+++ Grupp 2/Controllers/EducationController.cs
@@ -16,34 +16,16 @@ namespace Grupp_2.Controllers
     {
         private Datacontext db = new Datacontext();
 
-        // GET: Education
         public ActionResult Index()
         {
             return View(db.Educations.ToList());
         }
 
-        // GET: Education/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Education education = db.Educations.Find(id);
-            if (education == null)
-            {
-                return HttpNotFound();
-            }
-            return View(education);
-        }
-
-        // GET: Education/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "EduID,Title")] Education education)
@@ -54,7 +36,6 @@ namespace Grupp_2.Controllers
             {
                 if (ModelState.IsValid)
                 {
-
                     db.Educations.Add(education);
                     db.SaveChanges();
                     return RedirectToAction("Create");
@@ -62,71 +43,11 @@ namespace Grupp_2.Controllers
             }
 
             else if (tempEdu.Title.ToLower() == education.Title.ToLower())
-
             {
                 TempData["alertMessage"] = "Denna utbildning existerar redan i systemet!";
                 return RedirectToAction("Create");
             }
-
             return View(education);
-        }
-
-        public ActionResult CreateEduVM()
-        {
-            return View();
-        }
-
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Education education = db.Educations.Find(id);
-            if (education == null)
-            {
-                return HttpNotFound();
-            }
-            return View(education);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "EduID,Title")] Education education)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(education).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(education);
-        }
-
-        
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Education education = db.Educations.Find(id);
-            if (education == null)
-            {
-                return HttpNotFound();
-            }
-            return View(education);
-        }
-
-        // POST: Education/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Education education = db.Educations.Find(id);
-            db.Educations.Remove(education);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
