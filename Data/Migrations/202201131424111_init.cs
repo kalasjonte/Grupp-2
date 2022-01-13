3309f1b1936eb4c28148f28b191ce67a5f68cd65
Kalasjonte <jonathan.gabrielsson@live.se>
2022-01-13 15:28:48 +0100

    fitta

---
+++ Data/Migrations/202201131424111_init.cs
@@ -0,0 +1,22 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.CVs", "Clicks", c => c.Int(nullable: false));
+            AddColumn("dbo.Users", "Deactivate", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Users", "GithubUsername", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Users", "GithubUsername");
+            DropColumn("dbo.Users", "Deactivate");
+            DropColumn("dbo.CVs", "Clicks");
+        }
+    }
+}
